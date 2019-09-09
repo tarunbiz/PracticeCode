c80bdc045d67927f4758eab4dd2a04ccc8d76bac
tarunbiz <tarunbiz@gmail.com>
2019-09-09 15:58:13 -0400

    Uploading practice code to Github

---
+++ Organized/src/com/pluralsight/getorganized/Main.java
@@ -0,0 +1,18 @@
+package com.pluralsight.getorganized;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float students= 150;
+        float rooms = 0;
+
+        if (rooms > 0 && students/rooms > 30) {
+            System.out.println("Crowded!!");
+        }
+        //else
+        //    System.out.println("No students");
+
+        System.out.println();
+        System.out.println("** end program **");
+    }
+}
